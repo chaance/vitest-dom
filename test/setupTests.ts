18f8927627a8f58eca01ed0e9312f1e59bf1238e
Chance Strickland <hi@chance.dev>
2023-05-15 12:21:56 -0700

    skip calling `expect.extend` in test setup

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -1,7 +1,5 @@
 import { expect } from "vitest";
 import { plugins } from "pretty-format";
-import * as matchers from "../src/matchers";
 import "../src/extend-expect";
 
-expect.extend(matchers);
 expect.addSnapshotSerializer(plugins.ConvertAnsi as any);
