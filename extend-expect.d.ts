66bfe0e4879512d24ad343f5fbe4fdba931f6c72
Chance Strickland <hi@chance.dev>
2023-09-16 11:57:59 -0700

    extend-expect tweaks

--- extend-expect.d.ts
+++ extend-expect.d.ts
@@ -1,2 +1,10 @@
-import "./dist/extend-expect";
-export {};
+import "vitest";
+import type { expect } from "vitest";
+import type { TestingLibraryMatchers } from "./dist/matchers";
+
+declare module "vitest" {
+  interface Assertion<T = any>
+    extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
+  interface AsymmetricMatchersContaining
+    extends TestingLibraryMatchers<unknown, unknown> {}
+}
--- src/extend-expect.ts
+++ src/extend-expect.ts
@@ -2,11 +2,11 @@ import { expect } from "vitest";
 import * as matchers from "./matchers";
 import type { TestingLibraryMatchers } from "./matchers";
 
+expect.extend(matchers);
+
 declare module "vitest" {
   interface Assertion<T = any>
     extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
   interface AsymmetricMatchersContaining
     extends TestingLibraryMatchers<unknown, unknown> {}
 }
-
-expect.extend({ ...matchers });
